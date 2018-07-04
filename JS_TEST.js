dbec67d9df56d70cb671305469cc9fdf9c8ffd99
sodin925 <30490685+sodin925@users.noreply.github.com>
2018-07-05 01:48:06 +0800

    1

---
+++ JS_TEST.js
@@ -0,0 +1,8 @@
+var name = "Joe";
+var a = 0;
+while (a < 2) {
+    document.write("Happy Birthday <br>");
+    a = a + 1;
+}
+document.write("dear Joe <br>");
+document.write("Happy Birthday <br>");
