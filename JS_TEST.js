fa07956ea2aec3d3113ef97e703b665d129ecbba
sodin925 <30490685+sodin925@users.noreply.github.com>
2018-07-09 21:57:28 +0800

    立即函式

--- JS_TEST.js
+++ JS_TEST.js
@@ -1,8 +1,30 @@
-var name = "Joe";
-var a = 0;
-while (a < 2) {
-    document.write("Happy Birthday <br>");
-    a = a + 1;
-}
-document.write("dear Joe <br>");
-document.write("Happy Birthday <br>");
+// function bake(degrees){
+//     var message;
+//     if(degrees > 500){
+//         message="1";
+//     }
+//     else if(degrees < 100){
+//         message="2";
+//     }
+//     else{
+//         message="3";
+//         // console.log("123");
+//     }
+//     return message;
+// }
+// var a = bake(350);
+
+var ss = function (a) {
+    console.log(a);
+}(1);
+
+
+function m(a) {      //需命名
+    console.log(a); 
+};
+m(1);
+
+
+(function (a) {
+    console.log(a);
+})(1);
