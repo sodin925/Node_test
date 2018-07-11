da9d708aa8dca1bdf2b3da7461a90f6f6f8d27af
sodin925 <30490685+sodin925@users.noreply.github.com>
2018-07-11 17:39:26 +0800

    mime

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
+var mime = require("mime");
+var url = require("url");
 var fs = require("fs");
 var http = require("http");
 
-http.createServer(function(req, res){
-    res.writeHead(200, {
-        "content-type": "text/html"
+var server = http.createServer(function (req, res) {
+    var pathname = url.parse(req.url).pathname;
+    fs.stat("." + pathname, function (err, stats) {
+        if (!err && stats.isFile()) {
+            fs.readFile("./1.html", function (err, html) {
+                if (!err) {
+                    res.writeHead(200, {
+                        "content-type": mime.getType(pathname)
+                    });
+                    res.write(html);
+                    res.end();
+                }
+            });
+        }
     });
-    var html = fs.readFileSync("./1.html");
-    res.write(html);
-    res.end();
+
 }).listen(3000);
