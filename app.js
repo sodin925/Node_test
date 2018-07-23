0e745ba6d570440f4be904c271c6b0a7a444d1f1
sodin925 <30490685+sodin925@users.noreply.github.com>
2018-07-23 22:19:16 +0800

    OK

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
-var mime = require("mime");
+var mime = require("mime"); //自動判定content-type
 var url = require("url");
 var fs = require("fs");
 var http = require("http");
 
 var server = http.createServer(function (req, res) {
     var pathname = url.parse(req.url).pathname;
-    fs.stat("." + pathname, function (err, stats) {
+    if (pathname.endsWith("/")) {                  //預設頁
+        pathname += "index.html";
+    }
+    var relativePathname = decodeURIComponent((process.argv[2] || ".") + pathname);  //開啟參數
+    fs.stat(relativePathname, function (err, stats) {
+        if (!err && stats.isDirectory()) {
+            res.writeHead(302, {  //status code 302
+                "Location" : pathname + "/" + (url.parse(req.url).search || "")
+            });
+            res.end();
+            return;
+        }
         if (!err && stats.isFile()) {
-            fs.readFile("./1.html", function (err, html) {
+            fs.readFile(relativePathname, function (err, html) {
                 if (!err) {
                     res.writeHead(200, {
-                        "content-type": mime.getType(pathname)
+                        "Content-type": mime.getType(pathname)
                     });
                     res.write(html);
                     res.end();
                 }
             });
+        } else {
+            res.writeHead(404);
+            res.write("Not Found");
+            res.end();
         }
     });
-
 }).listen(3000);
